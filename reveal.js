f272a2c8820f886f61f565b16bbc853491a0c699
Kaique Lopes <lopes.kaique@outlook.com>
2023-11-05 21:53:08 -0300

    1commit

---
+++ reveal.js
@@ -0,0 +1,154 @@
+
+window.revelar = ScrollReveal({reset:true});
+
+//  TOPO PORTFOLIO
+
+revelar.reveal('.efeito-txt-topo',
+{
+    duration: 2000,
+    distance:'90px'
+})
+
+
+
+window.revelar = ScrollReveal({reset:true});
+
+//  TOPO PORTFOLIO
+
+revelar.reveal('.efeito-img-topo',
+{
+    duration: 2000,
+    distance:'90px',
+    delay: 500
+})
+
+
+// titulos section
+
+
+revelar.reveal('.titulo',
+{
+    duration: 2000,
+    distance:'90px'
+})
+
+// especialidades section
+
+revelar.reveal('.efeito-especialidades1',
+{
+    duration: 2200,
+    distance:'90px',
+    delay: 500
+})
+
+
+revelar.reveal('.efeito-especialidades2',
+{
+    duration: 2200,
+    distance:'90px',
+    delay: 700
+})
+
+
+revelar.reveal('.efeito-especialidades3',
+{
+    duration: 2200,
+    distance:'90px',
+    delay: 900
+})
+
+
+//  section sobre mim
+
+revelar.reveal('.efeito-img-sobre',
+{
+    duration: 2200,
+    distance:'90px',
+    delay: 700,
+    origin:'left'
+})
+
+revelar.reveal('.efeito-txt-sobre',
+{
+    duration: 3200,
+    distance:'90px',
+    delay: 900,
+    origin:'right'
+})
+
+
+//  section portfolio
+
+revelar.reveal('.img-port1',
+{
+    duration: 2000,
+    distance:'90px',
+    delay: 500
+})
+
+revelar.reveal('.img-port2',
+{
+    duration: 2000,
+    distance:'90px',
+    delay: 700
+})
+
+revelar.reveal('.img-port3',
+{
+    duration: 2000,
+    distance:'90px',
+    delay: 900
+})
+
+
+// section forms
+
+
+revelar.reveal('.btn-submit',
+{
+    duration: 2000,
+    distance:'90px',
+    delay: 1500
+})
+
+
+revelar.reveal('.nome1',
+{
+    duration: 2000,
+    distance:'90px',
+    delay: 700
+})
+
+revelar.reveal('.nome2',
+{
+    duration: 2000,
+    distance:'90px',
+    delay: 900
+})
+
+
+revelar.reveal('.nome3',
+{
+    duration: 2000,
+    distance:'90px',
+    delay: 1100
+})
+
+revelar.reveal('.nome4',
+{
+    duration: 2000,
+    distance:'90px',
+    delay: 1300
+})
+
+
+
+
+
+
+
+
+
+
+
+
